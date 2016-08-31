7ce8ab54d7c5597ba291410aebada7ad92084022
Kami Boers <kamiboers@gmail.com>
2016-08-31 11:44:10 -0600

    get and post requests to mongo

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
 var db
 
 MongoClient.connect('mongodb://kami:tonsai@ds019946.mlab.com:19946/kamdash', (err, database) => {
@@ -13,11 +16,13 @@ MongoClient.connect('mongodb://kami:tonsai@ds019946.mlab.com:19946/kamdash', (er
   })
 })
 
-app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-  var cursor = db.collection('tasks').find()
+  db.collection('tasks').find().toArray(function(err, result) {
+    if (err) return console.log(err)
+    console.log(result)
+    res.render('index.ejs', {tasks: result})
+  })
 })
 
 app.post('/todo', (req, res) => {
